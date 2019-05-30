70033e4a8b050707df793e4164da25a37990e9ca
fengy <2853105214@qq.com>
2019-05-30 14:51:28 +0800

    test commit

---
+++ day001/test01.py
@@ -0,0 +1,19 @@
+import turtle
+#初始化笔大小
+turtle.pensize(1)
+#笔颜色
+turtle.pencolor('#333')
+#移动距离
+turtle.forward(100)
+#偏转角度
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+#turtle.mainloop()
