1f4bdb9de8644a0e9d17003e2dae48ce28888d98
V <bladefy@gmail.com>
2021-12-09 10:46:40 +0800

    Delete day002 directory
    delete

--- day002/002-1.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-
-'''
-@name:xxx
-@author:xxx
-@email:xxx@xxx.com
-@version:0.0.1
-'''
--- day002/002-5.py
+++
@@ -1,4 +0,0 @@
-import skimage 
-img = skimage.io.imread('C:/Users/Administrator/Pictures/11.png')#调用io成员中的imread方法，读入一张图片
-skimage.io.imshow(img)  #调用io成员中的imshow方法，显示图片
-
