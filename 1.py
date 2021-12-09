577f4b0e3c63e4ed90e5535bb02249da1fa98260
Zhang San <zhangsan@fwkt.com>
2021-12-09 14:05:25 +0800

    1day

---
+++ 1.py
@@ -0,0 +1,28 @@
+#coding= utf-8
+
+'''
+@name:xxx
+@author:xxx
+@email:xxx@xxx.com
+@version:0.0.1
+'''
+
+import turtle
+#初始化笔大小
+turtle.pensize(1)
+#笔颜色
+turtle.pencolor('#333')
+#移动距离
+turtle.forward(100)
+#偏转角度
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+turtle.right(30)
+turtle.forward(100)
+#turtle.mainloop()
