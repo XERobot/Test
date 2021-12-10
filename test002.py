aedb3dd92eccdedc1918b1d1dd0f2bea7a9952cd
Zhang San <zhangsan@fwkt.com>
2021-12-10 18:21:00 +0800

    1

--- test002.py
+++ test002.py
@@ -8,6 +8,20 @@ b = 3
 #求模，余数
 #整除，结果整数
 #求幂
+#格式化 %.1f  %d  %s
 print(a%b)  
 print(a//b)
 print(a**b)
+
+
+k=float(input('Please input 开尔文:'))
+c= (k-32)/1.8
+print(c)
+
+format1 = 1.000000
+format2 = 4
+format3 = 'abcdefg'
+
+print('float类型：%2.2f' % (format1))
+print('int类型：%d' % (format2))
+print('str类型：%5.1s' % (format3))
---
+++ test003.py
@@ -0,0 +1,6 @@
+import math
+
+l = 10
+
+area = float(l*math.pi*math.pi)
+print('圆面积=%5.15f' %(area))
