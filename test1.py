b9ba2119d2f2f7ec9f3cf88a4741e35c5b56f3bf
Zhang San <zhangsan@fwkt.com>
2021-12-10 15:24:14 +0800

    变量类型

--- test1.py
+++ test1.py
@@ -1,6 +1,25 @@
 #coding= utf-8
-"""
+'''
 注意，编码声明coding和=不能有空格，否则不生效
 符号可以是=，可以是:
-"""
-print("结果")
+'''
+#Author : 张三
+#Version : 0.0.1
+
+a = 10
+b = 20
+print(a+b)
+print(a/b)
+
+print(int(a/b))
+'''
+int()
+float()
+str()
+chr()和ord()是互相转换，字符转编码，编码转字符
+
+'''
+c='我'
+d=25106
+print(ord(c))
+print(chr(d))
